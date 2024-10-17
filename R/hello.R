23f4d3b276165a845a8a83d0f9b1d544cd164ed2
Nick Grange <ngra9621@sydney.edu.au>
2024-10-17 14:44:10 +1100

    checking commit

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -16,3 +16,29 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+
+
+
+#' print function
+#'
+#' @param input This is the input, it accepts true or false values
+#'
+#' @return
+#' @export
+#'
+#' @examples print_function(input = TRUE)
+#' @examples print_function(input = FALSE)
+#'
+#'
+
+print_function <- function(input = TRUE) {
+  if(input == TRUE) {
+    print("the input is true")
+
+  } else if(input == FALSE) {
+    print("The input is false")
+  }
+}
+
+
